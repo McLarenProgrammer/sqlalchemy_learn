8eaba6da8a7d979d72fe4ba2e07acfba00956eb6
McLarenProgrammer <mtt.programmer@gmail.com>
2025-04-27 15:26:53 +0500

    Создание классов-таблиц Написал два класса User и Address, которые представляют собой таблицы user_account и address в базе данных, используя ORM API (Declarative Base, Mapped, mapped_column(), relationship() и т.д.)

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
+from typing import List, Optional
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import Session
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"))
+
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self):
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 postgre_engine = create_engine(
-    "postgresql+psycopg://rec-user:27099123zb@localhost:5432/recommendationsystem",
+    "postgresql+psycopg://rec-user:27099123zb@localhost:5432/learnsqlalchemy",
     echo=True
 )
 
-stmt = text("SELECT login, password_hash, is_cataloger FROM users WHERE is_cataloger = :isCataloger")
-with Session(postgre_engine) as session:
-    result = session.execute(stmt, {"isCataloger": True})
-    for row in result:
-        print(f"Login: {row.login} Password_hash: {row.password_hash} Cataloger: {row.is_cataloger}")
+Base.metadata.create_all(postgre_engine)
